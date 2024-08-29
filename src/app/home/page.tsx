a418e5d0be75deee930f70b04badefb9efea3f03
Julia dos Santos <santos.ju1995@gmail.com>
2024-08-28 23:30:15 -0300

    home

---
+++ src/app/home/page.tsx
@@ -0,0 +1,9 @@
+import Sidebar from "@/components/Sidebar";
+
+export default function Home() {
+    return (
+        <div className="h-screen w-screen bg-zinc-800">
+            <Sidebar/>
+        </div>
+    );
+}
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,34 @@
+import { CircleCheckBig, HomeIcon, LogOut, SquarePen } from "lucide-react";
+
+export default function Sidebar() {
+    return (
+        <aside className="bg-zinc-900 w-60 h-full flex-col pt-8">
+            <div className=" flex flex-col gap-5">
+                <h1 className="font-bold text-zinc-300 text-xl pl-5">ToDo App</h1>
+                <div className="h-0.5 w-full bg-zinc-600"></div>
+            </div>
+
+            <div className="flex flex-col gap-10 pl-5 pt-5 text-zinc-400">
+                <div className="hover:text-zinc-100 flex gap-2">
+                    <HomeIcon size={22}/>
+                    <a href="/home">Home</a>
+                </div>
+
+                <div className="hover:text-zinc-100 flex gap-2">
+                    <SquarePen size={22}/>
+                    <a href="">Nova tarefa</a>
+                </div>
+
+                <div className="hover:text-zinc-100 flex gap-2">
+                    <CircleCheckBig size={22}/>
+                    <a href="">Tarefas concluídas</a>
+                </div>
+
+                <div className="hover:text-zinc-100 flex gap-2">
+                    <LogOut size={22}/>
+                    <a href="/login">Sair</a>
+                </div>
+            </div>
+        </aside>
+    );
+}
